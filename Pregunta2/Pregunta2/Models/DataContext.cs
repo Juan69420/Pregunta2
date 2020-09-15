20b145f2a4057d53aafe918dc4c1a673100e7a9a
Juan Gianella Blanco <a2019116481@estudiantes.upsa.edu.bo>
2020-09-15 13:22:01 -0400

    Se ha creado una solución MVC alojada en la nube Azure que soporte el  modelo con JWT.

---
+++ Pregunta2/Pregunta2/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Pregunta2.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+        public System.Data.Entity.DbSet<Pregunta2.Models.Student>Students{ get; set; }
+        public System.Data.Entity.DbSet<Pregunta2.Models.Enrollment>Enrollments{ get; set; }
+        public System.Data.Entity.DbSet<Pregunta2.Models.Course>Courses{ get; set; }
+    }
+}
---
+++ Pregunta2/Pregunta2/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pregunta2.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Enrollmentdate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
